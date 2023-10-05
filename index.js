fb6709d4748ec8319afb6acc00fdc2f1ab9f1c74
Faith Muraya <111008831+faith-muraya@users.noreply.github.com>
2023-10-05 22:03:20 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const seen = new Set();
+
+  for (let num of array) {
+    const difference = target - num;
+
+    if (seen.has(difference)) {
+      return true;
+    }
+
+    seen.add(num);
+  }
+
+  return false;
+  e;
 }
 
 /* 
-  Write the Big O time complexity of your function here
+ The function iterates through the input array once, where 'n' is the length of the array.
+ The set operations (add and has) are constant time on average, so they do not significantly affect the overall time complexity.
 */
 
 /* 
-  Add your pseudocode here
+  Create an empty set called 'seen' to keep track of seen values.
+  Iterate through the elements 'num' in the input array.
+  Calculate 'difference' as 'target - num'.
+  Check if 'difference' is in the 'seen' set. If it is, return true.
+  Otherwise, add 'num' to the 'seen' set.
+  If the loop completes without finding a pair, return false.
 */
 
 /*
-  Add written explanation of your solution here
+  The function uses a set to efficiently keep track of the numbers seen so far while iterating through the array.For each element 'num', it calculates the 'difference' between the target and 'num'.If 'difference' is found in the 'seen' set, it means there exists a pair of numbers in the array that adds up to the target, so the function returns true If no such pair is found after iterating through the entire array, the function returns false.
+
 */
 
 // You can run `node index.js` to view these console logs
